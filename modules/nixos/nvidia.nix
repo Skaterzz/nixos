230b89018cbf2513bac3cead66618adf8d22b444
Joshua Randall <josh@joshrandall.net>
2026-07-31 22:15:07 -0400

    update nvidia driver

--- modules/nixos/nvidia.nix
+++ modules/nixos/nvidia.nix
@@ -20,6 +20,6 @@
     open = true;
 
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.production;
   };
 }

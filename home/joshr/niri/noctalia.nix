23bd75535055b7d157fae79a30cba53006d0961c
Joshua Randall <josh@joshrandall.net>
2026-08-08 00:55:21 -0400

    fixed

--- home/joshr/niri/noctalia.nix
+++ home/joshr/niri/noctalia.nix
@@ -742,20 +742,20 @@ let
         {
           action = "logout";
           label = "Log out";
-          countdown = 3;
+          countdown_seconds = 3;
           shortcut = "4";
         }
         {
           action = "reboot";
           label = "Reboot";
-          countdown = 5;
+          countdown_seconds = 5;
           shortcut = "5";
         }
         {
           action = "shutdown";
           label = "Power off";
           variant = "destructive";
-          countdown = 10;
+          countdown_seconds = 10;
           shortcut = "6";
         }
       ];
